8e9a0065ade8e20abbce867639b7f7917005cb05
OmarMorales0905 <omoralesibarra@gmail.com>
2017-05-07 00:38:41 -0500

    problem with json link so use local service instead

--- js/controllers/RestoCtrl.js
+++ js/controllers/RestoCtrl.js
@@ -1,5 +1,3 @@
-app.controller('RestoCtrl', function($scope, $http) {
-    $http.get("https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json").then(function(response) {
-        $scope.myData = response.data;
-    });
-});
+app.controller('RestosCtrl', ['$scope', 'Data', function($scope, Data){
+  $scope.restos = Data.all();
+}]);
