4a5a27809b1797ecfb87a09ca47d000d0b548fce
OmarMorales0905 <omoralesibarra@gmail.com>
2017-05-07 00:31:15 -0500

    get data from link

--- js/controllers/RestoCtrl.js
+++ js/controllers/RestoCtrl.js
@@ -1,3 +1,5 @@
-app.controller('RestoCtrl', ['$scope', function($scope){
-
-}]);
+app.controller('RestoCtrl', function($scope, $http) {
+    $http.get("https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json").then(function(response) {
+        $scope.myData = response.data;
+    });
+});
